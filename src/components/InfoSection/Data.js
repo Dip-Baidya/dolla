07112be33e6048396f00e25984b7e595efe3f67e
Dip-Baidya <dipbaidya.cse@gmail.com>
2022-02-18 02:57:41 +0600

    Complete Discover & SignUP Section

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,48 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premium Bank',
+    headLine: 'Unlimited Transactions with zero fees',
+    description: 'get access to our exclusive app that always you to send unlimited transaction without getting charged any fees.',
+    buttonLabel: 'Get started',
+    imgStart: false,
+    img: require('https://svgshare.com/i/eL5.svg'),
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+};
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Unlimited Access',
+    headLine: 'Login to your account at my time',
+    description: 'get access to our exclusive app that always you to send unlimited transaction without getting charged any fees.',
+    buttonLabel: 'Get started',
+    imgStart: false,
+    img: require('../../images/svg-1.svg'),
+    alt: 'PiggyBank',
+    dark: false,
+    primary: false,
+    darkText: true
+};
+export const homeObjThree = {
+    id: 'signup',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Join Our Team',
+    headLine: 'Creating an account is extremely easy',
+    description: 'Get Everything set up and ready in under 10 minutes. All you need to do is add your information and you are ready to go.',
+    buttonLabel: 'Start Now',
+    imgStart: false,
+    img: require('https://svgshare.com/i/eL5.svg'),
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { useState } from 'react';
+import Discover from '../components/Discover';
 import HeroSection from '../components/HeroSection';
+import InfoSection from '../components/InfoSection';
+import { homeObjOne, homeObjTwo, homeObjThree } from '../components/InfoSection/Data';
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
+import SignUp from '../components/SignUp';
 
 const Home = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -15,6 +19,11 @@ const Home = () => {
             <Sidebar isOpen={isOpen} toggle={toggle}></Sidebar>
             <Navbar toggle={toggle}></Navbar>
             <HeroSection></HeroSection>
+            <InfoSection {...homeObjOne}></InfoSection>
+            <Discover {...homeObjTwo}></Discover>
+            <SignUp {...homeObjThree}></SignUp>
+            
+
         </>
     );
 };
